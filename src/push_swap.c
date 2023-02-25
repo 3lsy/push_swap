23e45afdb293df15f743f859ff1680bd2e75c846
3lsy <echavez-@student.42.fr>
2023-02-25 12:49:00 +0100

    name makefile

---
+++ src/push_swap.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 18:29:48 by echavez-          #+#    #+#             */
+/*   Updated: 2023/02/25 12:06:13 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+** check if the input is not digit or -
+** numbers can't be duplicated
+*/
+
+int	main(int ac, char **av)
+{
+	ft_putstr_fd("\n", 1);
+	return (0);
+}
